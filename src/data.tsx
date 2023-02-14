f7dde83892608b9192786fb3a236bbd373e70edd
Ali Nategh <alinategh005@gmail.com>
2023-02-14 12:24:22 +0330

    fix: view ports and widths on mobile

--- src/data.tsx
+++ src/data.tsx
@@ -29,5 +29,35 @@ export default [
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl: "./Geirangerfjord.png"
     },
+    {
+        id: 4,
+        title: "Fount Muji",
+        location: "Pajan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "SCROLLING TEST.",
+        imageUrl: "./MountFuji.png"
+    },
+    {
+        id: 5,
+        title: "Hydney Spera Ouse",
+        location: "TAUSARALIA",
+        googleMapsUrl: "https://goo.gl/maps/",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        imageUrl: "./SydneyOpera.png"
+    },
+    {
+        id: 6,
+        title: "Feirangergjord",
+        location: "ANDWAY",
+        googleMapsUrl: "https://goo.gl/maps/",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "./Geirangerfjord.png"
+    },
 ]
 
